ee074b52753dc5a324ca14188a16f21d201f1cd9
0736b <thanawat_bumpengpun@elearning.cmu.ac.th>
2020-12-22 21:57:09 +0700

    PracticalExam01_01 completed

---
+++ PracticalExam01/PracticalExam01_01.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string hideVowels(string inputText){
+	int lengthText = inputText.size();
+	int n = 0;
+	
+	while(n<lengthText){
+		if(inputText[n]=='a' ||inputText[n]=='e' ||inputText[n]=='i' ||inputText[n]=='o' ||inputText[n]=='u' ){
+			inputText[n] = '_';
+		}
+		if(inputText[n]=='A' ||inputText[n]=='E' ||inputText[n]=='I' ||inputText[n]=='O' ||inputText[n]=='U'){
+			
+			inputText[n] = '_';
+			
+		}
+		n++;
+	}
+	
+	return inputText;
+	
+	
+}
+
+int main(){
+	
+	cout<<hideVowels("HELLO WORLD");
+	return 0;
+	
+	
+}
