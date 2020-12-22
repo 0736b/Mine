cae7b497ca2f0eacfed932c55f98e2d9ce4ab970
0736b <76035626+0736b@users.noreply.github.com>
2020-12-22 22:05:17 +0700

    Update PracticalExam01_01.cpp

--- PracticalExam01/PracticalExam01_01.cpp
+++ PracticalExam01/PracticalExam01_01.cpp
@@ -1,32 +1,24 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
 string hideVowels(string inputText){
 	int lengthText = inputText.size();
 	int n = 0;
-	
 	while(n<lengthText){
 		if(inputText[n]=='a' ||inputText[n]=='e' ||inputText[n]=='i' ||inputText[n]=='o' ||inputText[n]=='u' ){
 			inputText[n] = '_';
 		}
 		if(inputText[n]=='A' ||inputText[n]=='E' ||inputText[n]=='I' ||inputText[n]=='O' ||inputText[n]=='U'){
 			
-			inputText[n] = '_';
-			
+			inputText[n] = '_';	
 		}
 		n++;
 	}
-	
-	return inputText;
-	
-	
+	return inputText;	
 }
 
-int main(){
-	
+int main(){	
 	cout<<hideVowels("HELLO WORLD");
 	return 0;
-	
 	
 }
